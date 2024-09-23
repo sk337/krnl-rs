05421acde1127299693c3e339203fcda99325b13
sk337 <79583517+sk337@users.noreply.github.com>
2024-09-23 19:25:57 +0000

    fuck

---
+++ .devcontainer/install.sh
@@ -0,0 +1,8 @@
+sudo apt install qemu-system-x86_64 -y
+
+rustup toolchain install nightly
+rustup default nightly
+rustup target add x86_64-unknown-none
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+
+cargo install bootloader
---
+++ run.sh
@@ -0,0 +1,4 @@
+export CARGO_MANIFEST_DIR=$PWD
+export PATH=$PWD/target/debug/build/bootloader-abbe6ca3e0d61ff2/out/bin:$PATH
+
+bootimage runner target/x86_64/release/krnl-rs
